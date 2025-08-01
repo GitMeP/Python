5cc34e1b6972fdab09dc75893c5879337be10ca6
megha <meghapandey951@gmail.com>
2025-08-01 14:56:19 +0530

    first commit

---
+++ OOPs/python_oops.py
@@ -0,0 +1,7 @@
+# Object Orient Programming in Python
+
+# What is Object?
+# Its always there but you dont know that its there.
+# For Example : print(type("hello"))
+#             >> <class 'str'>
+
